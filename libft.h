34ed11a0d2da6a27a7393a2043510ee1abcb0afe
Selio30 <SelioBarbero@gmail.com>
2023-05-13 02:11:44 +0200

    add all

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sbarbero <sbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 18:06:52 by sbarbero          #+#    #+#             */
-/*   Updated: 2023/05/12 22:00:25 by sbarbero         ###   ########.fr       */
+/*   Updated: 2023/05/13 02:05:04 by sbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,5 +118,15 @@ typedef struct s_list
 
 // create a new list.
 t_list	*ft_lstnew(void *content);
+
+void	ft_lstadd_front(t_list **lst, t_list *new);
+
+int		ft_lstsize(t_list *lst);
+
+t_list	*ft_lstlast(t_list *lst);
+
+void	ft_lstadd_back(t_list **lst, t_list *new);
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
